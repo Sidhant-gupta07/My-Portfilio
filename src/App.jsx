c624b3f2c5a7d2a9bdd390d636a57f502d2fe729
Sidhant-gupta07 <sidhantguptabdk123@gmail.com>
2024-08-13 23:10:07 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-<div>
-  <h1 className='bg-rose-500 text-2xl font-semibold'>Hello world</h1>
-</div>
+      <div>
+        <h1 className="bg-rose-500 text-2xl font-semibold">Hello world</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
